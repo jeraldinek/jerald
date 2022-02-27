ae24883801f07c0742d9ead1f7f72ce4d3eeff33
jeraldinek <tsagoua@gmail.com>
2022-02-27 07:19:25 -0500

    new script to run on ec2

--- learn.sh
+++ learn.sh
@@ -1,2 +1,13 @@
+#!/bin/bash
+# use this for your user data ( script from top to bottom)
+# install httpd ( linux 2 version)
+yum update -y
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "<h1>hello world from $(hostname -f)<h1>"  > /var/www/html/index.html
+
+# this is a web example of ec2 instance in aws
+
 # learn.sh
-this repersitory is to store bash shell script
+#this repersitory is to store bash shell script
